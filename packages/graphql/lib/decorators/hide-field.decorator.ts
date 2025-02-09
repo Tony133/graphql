9beaabb3e6d323e9fb72d094be618477aafc7181
Antonio Tripodi <Tony133@users.noreply.github.com>
2025-02-09 11:58:23 +0100

    chore: update hide-field.decorator.ts

--- packages/graphql/lib/decorators/hide-field.decorator.ts
+++ packages/graphql/lib/decorators/hide-field.decorator.ts
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
 
 /**
  * @publicApi 
  */
-||||||| a1ae79e2
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-function */
-=======
->>>>>>> master
 export function HideField(): PropertyDecorator {
   return (target: Record<string, any>, propertyKey: string | symbol) => {};
 }
